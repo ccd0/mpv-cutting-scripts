454f7d40ce4c22c4134e278f34bd757c2cb83600
ccd0 <admin@containerchan.org>
2014-04-05 02:05:18 -0700

    remove sound for 4chan

--- encode.sh
+++ encode.sh
@@ -7,6 +7,6 @@ outname=$(basename "$filename")
 outname="${outname%.*} [$humanstart].webm"
 ln -s "$outname" clip.webm
 ffmpeg -i clip.mkv -crf 10 -b:v 4M -speed 0 -an -pass 1 -f webm -y /dev/null
-ffmpeg -i clip.mkv -crf 10 -b:v 4M -speed 0 -q:a 5 -pass 2 clip.webm
+ffmpeg -i clip.mkv -crf 10 -b:v 4M -speed 0 -an -pass 2 clip.webm
 popd
 
